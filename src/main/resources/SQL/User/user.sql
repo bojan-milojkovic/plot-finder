6a01b9a3b3a1bd4064727ebdd30808eb54100750
Bojan Milojko <lord_lazaruss@yahoo.com>
2019-01-27 17:21:31 +0100

    User table ddl

---
+++ src/main/resources/SQL/User/user.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `not_locked` tinyint(1) NOT NULL,
+  `reg_date` datetime NOT NULL,
+  `last_login` datetime NOT NULL,
+  `last_password_change` datetime NOT NULL,
+  `last_update` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
