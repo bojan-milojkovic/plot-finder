f774c9c974d3e0dedc1e082763313126a913115f
bojan.milojkovic <bojan.milojkovic@tlscontact.com>
2019-02-21 11:45:20 +0100

    BOJAN
    Prototype plot index

---
+++ src/main/resources/SQL/indexes/plot_indexes.sql
@@ -0,0 +1,16 @@
+CREATE BITMAP INDEX MyBitmapIndex ON plot(`power`,`water`,`gas`,`sewer`,`internet`,`garage`);
+
+CREATE INDEX CountryIndex ON plot(`country`);
+
+CREATE INDEX CityIndex ON plot(`city`);
+
+CREATE INDEX PriceIndex ON plot(`price`);
+
+CREATE INDEX SizeIndex ON plot(`size`);
+
+
+
+CREATE INDEX ll_x_index ON plot(`ll_x`);
+CREATE INDEX ll_y_index ON plot(`ll_y`);
+CREATE INDEX ur_x_index ON plot(`ur_x`);
+CREATE INDEX ur_y_index ON plot(`ur_y`);
