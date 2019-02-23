0c60df3c038941d7d96022f5cfdbe1453696a54f
Bojan Milojko <lord_lazaruss@yahoo.com>
2019-02-23 11:18:45 +0100

    BOJAN
    Final plot table version

--- src/main/resources/SQL/plot_update_1/plot_update_1.sql
+++ src/main/resources/SQL/plot_update_1/plot_update_1.sql
@@ -7,3 +7,19 @@ ADD COLUMN `farming` TINYINT(1) NOT NULL DEFAULT 0 AFTER `house`,
 ADD COLUMN `grazing` TINYINT(1) NOT NULL DEFAULT 0 AFTER `farming`,
 ADD COLUMN `orchard` TINYINT(1) NOT NULL DEFAULT 0 AFTER `grazing`,
 CHANGE COLUMN `description` `description` TEXT NOT NULL ;
+
+ALTER TABLE plotfinder.plot
+DROP COLUMN `country`,
+DROP COLUMN `power`,
+DROP COLUMN `water`,
+DROP COLUMN `gas`,
+DROP COLUMN `sewer`,
+DROP COLUMN `internet`,
+DROP COLUMN `garage`,
+DROP COLUMN `type`,
+DROP COLUMN `house`,
+DROP COLUMN `farming`,
+DROP COLUMN `grazing`,
+DROP COLUMN `orchard`,
+DROP COLUMN `num_pic`
+;
