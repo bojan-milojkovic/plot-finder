7804afde906cc77f0d9f43ea230f8cb303b4ddc8
bojan.milojkovic <bojan.milojkovic@tlscontact.com>
2019-02-26 14:14:45 +0100

    BOJAN
    Fixing "check if new plot is in area"

--- src/main/java/com/plot/finder/watched/repository/WatchedRepository.java
+++ src/main/java/com/plot/finder/watched/repository/WatchedRepository.java
@@ -9,7 +9,10 @@ import com.plot.finder.watched.entity.WatchedJPA;
 @Repository
 public interface WatchedRepository extends JpaRepository<WatchedJPA, Long>{
 
-	@Query("SELECT w FROM WatchedJPA w where (w.ll_x <= ?1 AND w.ur_x >= ?1) OR (w.ll_x <= ?3 AND w.ur_x >= ?3) OR "+
-											"(w.ll_y <= ?2 AND w.ur_y >= ?2) OR (w.ll_y <= ?4 AND w.ur_y >= ?4)")
+	// check at least one plot vertice is inside the watched area
+	@Query("SELECT w FROM WatchedJPA w where (w.ll_x<=?1 AND w_ur_x>=?1 AND w.ll_y<=?2 AND AND w.ur_y>=?2) OR "+ // (ll_x, ll_y) in area
+											"(w.ll_x<=?1 AND w_ur_x>=?1 AND w.ll_y<=?4 AND AND w.ur_y>=?4) OR "+ // (ll_x, ur_y) in area
+											"(w.ll_x<=?3 AND w_ur_x>=?3 AND w.ll_y<=?2 AND AND w.ur_y>=?2) OR "+ // (ur_x, ll_y) in area
+											"(w.ll_x<=?3 AND w_ur_x>=?3 AND w.ll_y<=?4 AND AND w.ur_y>=?4)")     // (ur_x, ur_y) in area
 	List<WatchedJPA> findAreasWatchingPlot(float ll_x, float ll_y, float ur_x, float ur_y);
 }
