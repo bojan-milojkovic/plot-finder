05ecaafbfc2f46e92feb18898bde976d8208908f
bojan.milojkovic <bojan.milojkovic@tlscontact.com>
2019-02-28 10:11:43 +0100

    BOJAN
    Optimizing WatchedArea query

--- src/main/java/com/plot/finder/watched/repository/WatchedRepository.java
+++ src/main/java/com/plot/finder/watched/repository/WatchedRepository.java
@@ -10,9 +10,21 @@ import com.plot.finder.watched.entity.WatchedJPA;
 public interface WatchedRepository extends JpaRepository<WatchedJPA, Long>{
 
 	// check at least one plot vertice is inside the watched area
-	@Query("SELECT w FROM WatchedJPA w where (w.ll_x<=?1 AND w.ur_x>=?1 AND w.ll_y<=?2 AND w.ur_y>=?2) OR "+ // (ll_x, ll_y) in area
+	/*
+	 @Query("SELECT w FROM WatchedJPA w where (w.ll_x<=?1 AND w.ur_x>=?1 AND w.ll_y<=?2 AND w.ur_y>=?2) OR "+ // (ll_x, ll_y) in area
 											"(w.ll_x<=?1 AND w.ur_x>=?1 AND w.ll_y<=?4 AND w.ur_y>=?4) OR "+ // (ll_x, ur_y) in area
 											"(w.ll_x<=?3 AND w.ur_x>=?3 AND w.ll_y<=?2 AND w.ur_y>=?2) OR "+ // (ur_x, ll_y) in area
-											"(w.ll_x<=?3 AND w.ur_x>=?3 AND w.ll_y<=?4 AND w.ur_y>=?4)")     // (ur_x, ur_y) in area
+											"(w.ll_x<=?3 AND w.ur_x>=?3 AND w.ll_y<=?4 AND w.ur_y>=?4)")     // (ur_x, ur_y) in area 
+	*	first query transformation :
+						...	where ((w.ll_x<=?1 AND w.ur_x>=?1) AND ((w.ll_y<=?2 AND w.ur_y>=?2) OR (w.ll_y<=?4 AND w.ur_y>=?4))) OR
+								  ((w.ll_x<=?3 AND w.ur_x>=?3) AND ((w.ll_y<=?2 AND w.ur_y>=?2) OR (w.ll_y<=?4 AND w.ur_y>=?4)))
+								 
+	*	second query transformation :
+						... where ((w.ll_x<=?1 AND w.ur_x>=?1) OR (w.ll_x<=?3 AND w.ur_x>=?3)) AND
+								  ((w.ll_y<=?2 AND w.ur_y>=?2) OR (w.ll_y<=?4 AND w.ur_y>=?4))
+		
+	*/
+	@Query("SELECT w FROM WatchedJPA w where ((w.ll_x<=?1 AND w.ur_x>=?1) OR (w.ll_x<=?3 AND w.ur_x>=?3)) AND "+
+											  "((w.ll_y<=?2 AND w.ur_y>=?2) OR (w.ll_y<=?4 AND w.ur_y>=?4))")
 	List<WatchedJPA> findAreasWatchingPlot(float ll_x, float ll_y, float ur_x, float ur_y);
 }
