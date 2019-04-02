589d2b5b804c3db9ba1a4f28121b94c56d6cb30b
Bojan Milojko <lord_lazaruss@yahoo.com>
2019-04-02 22:05:14 +0200

    BOJAN
    pages

--- src/main/resources/SQL/plotfinder_db.sql
+++ src/main/resources/SQL/plotfinder_db.sql
@@ -108,7 +108,7 @@ CREATE TABLE `user_has_roles` (
 
 LOCK TABLES `user_has_roles` WRITE;
 /*!40000 ALTER TABLE `user_has_roles` DISABLE KEYS */;
-INSERT INTO `user_has_roles` VALUES (1,1,1),(2,1,2);
+INSERT INTO `user_has_roles` VALUES (1,1,1),(2,1,2),(3,1,3);
 /*!40000 ALTER TABLE `user_has_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -122,3 +122,65 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-02-04 22:41:02
+
+DROP TABLE IF EXISTS `flags`;
+
+CREATE TABLE `flags` (
+  `flag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `plot_id` int(11) NOT NULL,
+  `flag` varchar(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`flag_id`),
+  UNIQUE KEY `flag_id` (`flag_id`),
+  KEY `CountryIndex` (`plot_id`,`flag`),
+  CONSTRAINT `fk_plot_foreign_key` FOREIGN KEY (`plot_id`) REFERENCES `plot` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `plot`;
+
+CREATE TABLE `plot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `polygon` varchar(150) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` text NOT NULL,
+  `address1` varchar(45) NOT NULL,
+  `address2` varchar(45) DEFAULT NULL,
+  `district` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `country` varchar(45) NOT NULL,
+  `price` int(11) NOT NULL,
+  `currency` varchar(3) NOT NULL,
+  `size` int(3) NOT NULL,
+  `added` date NOT NULL,
+  `expires` date NOT NULL,
+  `size_unit` varchar(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `my_foreign_key_user_idx` (`user_id`),
+  KEY `PriceIndex` (`price`),
+  KEY `SizeIndex` (`size`),
+  KEY `CityIndex` (`city`),
+  KEY `DistrictIndex` (`district`),
+  CONSTRAINT `my_foreign_key_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `watched_area`;
+
+CREATE TABLE `watched_area` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ll_x` decimal(9,6) NOT NULL,
+  `ll_y` decimal(9,6) NOT NULL,
+  `ur_x` decimal(9,6) NOT NULL,
+  `ur_y` decimal(9,6) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `plot_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ll_x_index` (`ll_x`),
+  KEY `ll_y_index` (`ll_y`),
+  KEY `ur_x_index` (`ur_x`),
+  KEY `ur_y_index` (`ur_y`),
+  KEY `user_table_fk` (`user_id`),
+  KEY `plot_table_fk` (`plot_id`),
+  CONSTRAINT `plot_table_fk` FOREIGN KEY (`plot_id`) REFERENCES `plot` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_table_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
