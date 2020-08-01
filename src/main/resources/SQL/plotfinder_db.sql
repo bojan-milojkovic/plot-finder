f81ffb80bef8af30eee55159ec4f3c2c7046226b
Bojan Milojko <lord_lazaruss@yahoo.com>
2020-08-01 21:00:52 +0200

    BOJAN
    Fixing database

--- src/main/resources/SQL/plotfinder_db.sql
+++ src/main/resources/SQL/plotfinder_db.sql
@@ -28,6 +28,7 @@ CREATE TABLE `plot` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `polygon` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `address1` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -60,7 +61,7 @@ CREATE TABLE `plot` (
 
 LOCK TABLES `plot` WRITE;
 /*!40000 ALTER TABLE `plot` DISABLE KEYS */;
-INSERT INTO `plot` (`id`, `polygon`, `user_id`, `title`, `description`, `address1`, `address2`, `district`, `city`, `country`, `price`, `currency`, `size`, `added`, `expires`, `size_unit`, `flags`) VALUES (10,'41.883907#-85.30149@41.638058#-85.31248@41.613422#-84.99387@41.843#-84.94993@',1,'tttt','rwtrtsf','hdgdgsh',NULL,NULL,'hdhrgdh','hdfgf',12556754,'EUR',42,'2019-02-23','2019-04-02','m2','0111100000000'),(13,'42.69014#-85.83543@42.374405#-85.91233@42.38252#-85.47288@42.66591#-85.56077@',1,'Usvojite me !!!','yyhsetg','dsdfgds',NULL,NULL,'tgghdfg','Serbia',364,'EUR',2100,'2019-04-18','2019-05-18','Ar','1000000000001'),(15,'42.360603#-86.10789@42.21431#-86.12986@42.222446#-85.91013@42.352486#-85.91013@',1,'Latest plot','fhdfgdfg','asdfsd',NULL,'gdsgsdfg','gdfsghsdfg','Srbija',364,'EUR',4300,'2019-04-30','2019-05-30','Ar','1000000100001');
+INSERT INTO `plot` (`id`, `polygon`, `user_id`, `phone`, `title`, `description`, `address1`, `address2`, `district`, `city`, `country`, `price`, `currency`, `size`, `added`, `expires`, `size_unit`, `flags`) VALUES (10,'41.883907#-85.30149@41.638058#-85.31248@41.613422#-84.99387@41.843#-84.94993@',1,'060 337 12 45','tttt','rwtrtsf','hdgdgsh',NULL,NULL,'hdhrgdh','hdfgf',12556754,'EUR',42,'2019-02-23','2019-04-02','m2','0111100000000'),(13,'42.69014#-85.83543@42.374405#-85.91233@42.38252#-85.47288@42.66591#-85.56077@',1,'060 337 12 45','Usvojite me !!!','yyhsetg','dsdfgds',NULL,NULL,'tgghdfg','Serbia',364,'EUR',2100,'2019-04-18','2019-05-18','Ar','1000000000001'),(15,'42.360603#-86.10789@42.21431#-86.12986@42.222446#-85.91013@42.352486#-85.91013@',1,'060 337 12 45','Latest plot','fhdfgdfg','asdfsd',NULL,'gdsgsdfg','gdfsghsdfg','Srbija',364,'EUR',4300,'2019-04-30','2019-05-30','Ar','1000000100001');
 /*!40000 ALTER TABLE `plot` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,11 +76,7 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `first_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone1` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone2` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `active` tinyint(1) NOT NULL,
   `not_locked` tinyint(1) NOT NULL,
   `reg_date` datetime NOT NULL,
@@ -101,7 +98,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`user_id`, `username`, `password`, `first_name`, `last_name`, `email`, `phone1`, `phone2`, `active`, `not_locked`, `reg_date`, `last_login`, `last_password_change`, `last_update`, `identifier`, `authorities`) VALUES (1,'Lazaruss','$2a$10$AjG7dmyZbFW8mIcHVCoJAet62tmMvygjatoT0dsGZInPAiiKc4XK6','Bojan','Milojkovic','lord_lazaruss@yahoo.com','0603771245',NULL,1,1,'2019-02-04 22:26:08','2019-06-10 21:45:23','2019-02-04 22:26:08','2019-02-04 22:26:08',NULL,'ROLE_USER#ROLE_ADMIN#ROLE_SUPERADMIN'),(3,'Kale01','$2a$10$S8.heFA539GoDdf/aVtsRezwdidrATzUZ2kWTOczOwO8pq/Y4iAF2','Davor','Milojkovic','lord_lazaruss@gmail.com','0605718733',NULL,1,1,'2019-03-10 13:43:29','2019-03-10 13:43:29','2019-03-10 13:43:29','2019-03-17 11:59:22',NULL,'ROLE_USER'),(4,'kanlic','$2a$10$0uKzUSjpgqy02OBvbovJ7eu0syZ2edUSnwN/p25h4WBXxNpzdRKha','Dusan','Kanlic','lord.lazaruss@gmail.com','0603771247',NULL,1,0,'2019-04-24 20:27:00','2019-04-24 20:27:00','2019-04-24 20:27:00','2019-06-09 13:23:02','6a13e3a0-e829-4a0c-9e42-922a1e6ef2ca','ROLE_USER'),(5,'LazarDj','$2a$10$EWZFRmCDXCu8.pxDAKlHoOUPEhxQtEA6XoS/hQupWDrchpRX1sFB2','Lazar','Djordjevic','djordjeviclazar90@gmail.com','0601578744',NULL,1,1,'2019-06-08 22:35:52','2019-06-08 22:35:52','2019-06-08 22:35:52','2019-06-08 22:35:52',NULL,'ROLE_USER');
+INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `active`, `not_locked`, `reg_date`, `last_login`, `last_password_change`, `last_update`, `identifier`, `authorities`) VALUES (1,'Lazaruss','$2a$10$AjG7dmyZbFW8mIcHVCoJAet62tmMvygjatoT0dsGZInPAiiKc4XK6','lord_lazaruss@yahoo.com',1,1,'2019-02-04 22:26:08','2020-08-01 20:26:00','2019-02-04 22:26:08','2019-02-04 22:26:08',NULL,'ROLE_USER#ROLE_ADMIN#ROLE_SUPERADMIN'),(3,'Kale01','$2a$10$S8.heFA539GoDdf/aVtsRezwdidrATzUZ2kWTOczOwO8pq/Y4iAF2','lord_lazaruss@gmail.com',1,1,'2019-03-10 13:43:29','2019-03-10 13:43:29','2019-03-10 13:43:29','2019-03-17 11:59:22',NULL,'ROLE_USER'),(4,'kanlic','$2a$10$0uKzUSjpgqy02OBvbovJ7eu0syZ2edUSnwN/p25h4WBXxNpzdRKha','lord.lazaruss@gmail.com',1,0,'2019-04-24 20:27:00','2019-04-24 20:27:00','2019-04-24 20:27:00','2019-06-09 13:23:02','6a13e3a0-e829-4a0c-9e42-922a1e6ef2ca','ROLE_USER'),(5,'LazarDj','$2a$10$EWZFRmCDXCu8.pxDAKlHoOUPEhxQtEA6XoS/hQupWDrchpRX1sFB2','djordjeviclazar90@gmail.com',1,1,'2019-06-08 22:35:52','2019-06-08 22:35:52','2019-06-08 22:35:52','2019-06-08 22:35:52',NULL,'ROLE_USER');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-10 21:56:33
+-- Dump completed on 2020-08-01 20:58:46
